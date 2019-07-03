1a62e44f828a181a4d65f31ff7b73f8c5b1b36f1
marco <marco.picasso@hotmail.it>
2019-07-03 20:49:12 +0200

    Swagger annotations

--- src/Controller/DocumentController.php
+++ src/Controller/DocumentController.php
@@ -2,19 +2,38 @@
 
 namespace App\Controller;
 
-
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use FOS\RestBundle\Controller\ControllerTrait;
+use Swagger\Annotations as SWG;
 
 class DocumentController extends AbstractController
 {
 
     use ControllerTrait;
+
+    private $repository;
+
+    function __construct()
+    {
+
+        
+    };
+
+
+
     /**
      * @return FOS\RestBundle\View\View
-     * @Rest\Get("/document")
+     * @Rest\Get("/document", name="get_document")
+     * @SWG\Get(
+     *      tags={"Document"},
+     *      summary="Gets the all documents",
+     *      consumes={"application/json"},
+     *      produces={"application/json"}
+     *      @SWG\Response(response=200, description="Return when succcessful"),
+     *      @SWG\Response(response=404, description="Returned when document is not found").
+     * )
      */
     public function getDocument()
     {
